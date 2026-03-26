c68edfba0cb52baa33a3e4df5913c4d408b546d3
jekro <157567675+jekr0@users.noreply.github.com>
2026-03-27 02:33:15 +0300

    Add floating create-post button in bottom-right corner

---
+++ src/views/components/create-post-modal_cp.php
@@ -0,0 +1,32 @@
+<div class="create-post-modal create-post-modal--hidden" id="create-post-modal" aria-hidden="true">
+    <div class="create-post-modal__panel" role="dialog" aria-modal="true" aria-labelledby="create-post-modal-title">
+        <h2 class="create-post-modal__title" id="create-post-modal-title">Новый пост</h2>
+
+        <div
+            class="create-post-modal__upload-dropzone"
+            data-component="post-upload-dropzone"
+            role="button"
+            tabindex="0"
+            aria-label="Загрузить изображение"
+        >
+            <input
+                class="create-post-modal__file-input"
+                type="file"
+                accept="image/png,image/jpeg,image/gif"
+                data-component="post-upload-input"
+            >
+
+            <div class="create-post-modal__upload-placeholder" data-component="post-upload-placeholder">
+                <img src="assets/images/icons/upload.svg" alt="Upload" class="create-post-modal__upload-icon">
+                <span class="create-post-modal__upload-label">Загрузить*</span>
+            </div>
+
+            <img class="create-post-modal__preview create-post-modal__preview--hidden" data-component="post-upload-preview" alt="Предпросмотр изображения">
+        </div>
+
+        <p class="create-post-modal__hint">Поддерживаются PNG, JPEG и GIF до 20 МБ.</p>
+    </div>
+</div>
+
+
+<button class="create-post-open-button" data-component="create-post-open" aria-label="Создать пост">+</button>
