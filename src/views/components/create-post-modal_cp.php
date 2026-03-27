5cb88a274e1fab3dc2cd2ca89f76f2c556ba26ea
jekro <157567675+jekr0@users.noreply.github.com>
2026-03-27 18:22:34 +0300

    Refine create post modal layout and move home add button styles

--- src/views/components/create-post-modal_cp.php
+++ src/views/components/create-post-modal_cp.php
@@ -1,32 +1,40 @@
 <div class="create-post-modal create-post-modal--hidden" id="create-post-modal" aria-hidden="true">
     <div class="create-post-modal__panel" role="dialog" aria-modal="true" aria-labelledby="create-post-modal-title">
-        <h2 class="create-post-modal__title" id="create-post-modal-title">Новый пост</h2>
-
-        <div
-            class="create-post-modal__upload-dropzone"
-            data-component="post-upload-dropzone"
-            role="button"
-            tabindex="0"
-            aria-label="Загрузить изображение"
-        >
-            <input
-                class="create-post-modal__file-input"
-                type="file"
-                accept="image/png,image/jpeg,image/gif"
-                data-component="post-upload-input"
+        <div class="create-post-modal__upload-column">
+            <div
+                class="create-post-modal__upload-dropzone"
+                data-component="post-upload-dropzone"
+                role="button"
+                tabindex="0"
+                aria-label="Загрузить изображение"
             >
+                <input
+                    class="create-post-modal__file-input"
+                    type="file"
+                    accept="image/png,image/jpeg,image/gif"
+                    data-component="post-upload-input"
+                >
 
-            <div class="create-post-modal__upload-placeholder" data-component="post-upload-placeholder">
-                <img src="assets/images/icons/upload.svg" alt="Upload" class="create-post-modal__upload-icon">
-                <span class="create-post-modal__upload-label">Загрузить*</span>
+                <div class="create-post-modal__upload-placeholder" data-component="post-upload-placeholder">
+                    <img src="assets/images/icons/upload.svg" alt="Upload" class="create-post-modal__upload-icon">
+                    <span class="create-post-modal__upload-label">Загрузить*</span>
+                </div>
+
+                <img class="create-post-modal__preview create-post-modal__preview--hidden" data-component="post-upload-preview" alt="Предпросмотр изображения">
             </div>
 
-            <img class="create-post-modal__preview create-post-modal__preview--hidden" data-component="post-upload-preview" alt="Предпросмотр изображения">
+            <p class="create-post-modal__hint">Поддерживаются PNG, JPEG и GIF до 20 МБ.</p>
         </div>
 
-        <p class="create-post-modal__hint">Поддерживаются PNG, JPEG и GIF до 20 МБ.</p>
+        <div class="create-post-modal__meta-column">
+            <h2 class="create-post-modal__title" id="create-post-modal-title">Новый пост</h2>
+            <input
+                class="create-post-modal__caption-input"
+                type="text"
+                name="post-title"
+                placeholder="Введите заголовок поста"
+                autocomplete="off"
+            >
+        </div>
     </div>
 </div>
-
-
-<button class="create-post-open-button" data-component="create-post-open" aria-label="Создать пост">+</button>
--- src/views/pages/home_pg.php
+++ src/views/pages/home_pg.php
@@ -1,27 +1,10 @@
 <?php
     // ЗАГЛУШКА (временное отображение трёх шрифтов)
 ?>
-<style>
-    .font-demo {
-        text-align: center;
-        margin: 2rem 0;
-    }
-    .font-1 {
-        font-family: 'MC_caps';
-        font-size: 40px;
-    }
-    .font-2 {
-        font-family: "SF_def";
-        font-size: 32px;
-    }
-    .font-3 {
-        font-family: "SF_def";
-        font-size: 24px;
-    }
-</style>
-
 <div class="font-demo">
     <p class="font-1">Can you fly? Летать умеешь?</p>
     <p class="font-2">Can you fly? Летать умеешь?</p>
     <p class="font-3">Can you fly? Летать умеешь?</p>
 </div>
+
+<button class="create-post-open-button" data-component="create-post-open" aria-label="Создать пост">+</button>
