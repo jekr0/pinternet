ed87a8208a4cebad043f46ce6b920496fbad9dc4
jekro <157567675+jekr0@users.noreply.github.com>
2026-03-31 18:28:47 +0300

    Adjust footer to gradient-only and restore post button position

---
+++ src/views/layouts/footer_lo.php
@@ -0,0 +1,4 @@
+<div class="footer-background" aria-hidden="true"></div>
+<footer class="footer">
+    <button class="create-post-open-button" data-component="create-post-open" aria-label="Создать пост">+</button>
+</footer>
