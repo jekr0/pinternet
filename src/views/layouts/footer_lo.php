0cb70796c2b8f88badd865f89fb42a1851853ddd
jekro <157567675+jekr0@users.noreply.github.com>
2026-04-07 16:04:21 +0300

    чистка footer

--- src/views/layouts/footer_lo.php
+++ src/views/layouts/footer_lo.php
@@ -1,4 +1,3 @@
-<div class="footer-background" aria-hidden="true"></div>
 <footer class="footer">
     <button class="create-post-open-button" data-component="create-post-open" aria-label="Создать пост">+</button>
 </footer>
