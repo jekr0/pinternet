cac22a00abe0c382727fb84ae247bfd100cfe439
jekro <157567675+jekr0@users.noreply.github.com>
2026-05-21 00:39:49 +0300

    Update post-full comment delete actions and rail alignment

--- src/views/components/post-full_cp.php
+++ src/views/components/post-full_cp.php
@@ -193,6 +193,11 @@ if (!$selectedPost) {
                                     <button class="post-full__comment-action-button<?php echo $commentIsOwner ? ' post-full__comment-action-button--edit' : ''; ?>" type="button" data-action="<?php echo $commentIsOwner ? 'comment-edit' : 'comment-report'; ?>" aria-label="<?php echo $commentIsOwner ? 'Редактировать комментарий' : 'Пожаловаться на комментарий'; ?>">
                                         <span class="post-full__comment-action-icon" data-svg-src="<?php echo $commentIsOwner ? '/assets/images/icons/S-edit.svg' : '/assets/images/icons/S-warning.svg'; ?>" aria-hidden="true"></span>
                                     </button>
+                                    <?php if ($commentIsOwner): ?>
+                                        <button class="post-full__comment-action-button post-full__comment-action-button--delete" type="button" data-action="comment-delete" aria-label="Удалить комментарий">
+                                            <span class="post-full__comment-action-icon" data-svg-src="/assets/images/icons/S-bin.svg" aria-hidden="true"></span>
+                                        </button>
+                                    <?php endif; ?>
                                 </div>
                                 <?php if (!$isReply && !empty($childrenRows)): ?>
                                     <div class="post-full__comment-children">
